6735c6547f28473a876e74693897216fd4fa04c5
Aishwarya Nandishettar <aishushettar95@gmail.com>
2026-04-16 21:48:24 +0500

    added vercel config

--- HRMS-Frontend/vite.config.js
+++ HRMS-Frontend/vite.config.js
@@ -2,20 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+   base: "/", // ✅ ADD THIS LINE
   plugins: [react()],
   define: {
     global: "window", // ✅ FIX for sockjs-client
   },
   server: {
     port: 5176,
-    host: true, // ✅ Allow external access
-    allowedHosts: [
-      'localhost',
-      '.ngrok-free.dev',
-      '.ngrok.io',
-      '.ngrok.app',
-      'trowel-eldercare-scouting.ngrok-free.dev'
-    ],
     proxy: {
       "/api": {
         target: `${process.env.VITE_API_BASE_URL}`,
