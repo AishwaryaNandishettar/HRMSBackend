5eb280ac908742ae0c117f8da94881699eee1539
Aishwarya Nandishettar <aishushettar95@gmail.com>
2026-04-18 12:08:39 +0500

    Add new Vercel URL to CORS configuration

--- HRMS-Backend/src/main/java/com/omoikaneinnovation/hmrsbackend/security/CorsConfig.java
+++ HRMS-Backend/src/main/java/com/omoikaneinnovation/hmrsbackend/security/CorsConfig.java
@@ -24,7 +24,8 @@ public class CorsConfig {
         config.setAllowedOrigins(List.of(
                 "http://localhost:5176",
                 "http://localhost:5173",
-                "https://hrmsfrontendapp2.vercel.app"
+                "https://hrmsfrontendapp2.vercel.app",
+                "https://hrms-frontend-final-nine.vercel.app"
         ));
 
         
