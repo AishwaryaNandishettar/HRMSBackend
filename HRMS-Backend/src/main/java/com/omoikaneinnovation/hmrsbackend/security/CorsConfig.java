e7325524f583145f8e22b36e38ab1f35b9a2238c
Aishwarya Nandishettar <aishushettar95@gmail.com>
2026-04-22 09:17:11 +0500

    fix @Value import build error

--- HRMS-Backend/src/main/java/com/omoikaneinnovation/hmrsbackend/security/CorsConfig.java
+++ HRMS-Backend/src/main/java/com/omoikaneinnovation/hmrsbackend/security/CorsConfig.java
@@ -1,6 +1,7 @@
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.beans.factory.annotation.Value;
 
 @Configuration
 public class CorsConfig {
