d2dc2dd6a8b58673c332a4ea06be1718f73edc29
Aishwarya Nandishettar <aishushettar95@gmail.com>
2026-04-22 18:58:28 +0500

    Fix: add global responsive CSS for all pages - fix overflow on deployed URL

--- HRMS-Frontend/src/main.jsx
+++ HRMS-Frontend/src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AuthProvider } from "./Context/Authcontext";
+import "./responsive.css";
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
